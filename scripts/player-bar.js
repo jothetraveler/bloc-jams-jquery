b5891e1e70084eb152438ed480b79948263b10bb
Mojo Codess <jcakelley@gmail.com>
2017-11-29 19:49:50 -0500

    Checkpoint 14 complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
   $('button#next').on('click', function(){
@@ -10,7 +10,7 @@
     const nextSongIndex = currentSongIndex + 1;
     if(nextSongIndex >= album.songs){return;}
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
   $('button#previous').on('click', function(){
     if(player.playState !== 'playing'){return;}
@@ -19,7 +19,23 @@
     const previousSongIndex = currentSongIndex - 1;
      if(previousSongIndex <= album.songs){return;}
      const previousSong = album.songs[previousSongIndex];
-     player.playPause(previousSong);
+     helper.playPauseAndUpdate(previousSong);
   });
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if(player.playState !== 'playing'){return;}
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text(currentTime);
+    $('#time-control input').val(percent);
+  }, 1000);
+
+  $('#volume-control input').on('input', event => {
+    player.setVolume(event.target.value);
+  });
 }
